94e433fcb8b80076f2e813cf0a3a1aeb595c949d
Administrator <bvannoy@medfusionsvs.com>
2013-09-03 16:19:33 -0500

    Removing comment from enumFilter.php

--- classes/local/filters/enumFilter.php
+++ classes/local/filters/enumFilter.php
@@ -2,8 +2,6 @@
 class enumFilter extends FilterBase {	
 	public static function filter($value, $options = array(), &$report, &$row) {
 
-		//if(!$value->getValue()) return $value;
-
 		$key = $value->getValue();
 
 		if (array_key_exists($key, $options)) {
